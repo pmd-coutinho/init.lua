3d8e526e172cfc5d4deda1b135ef7fd9411c074a
Pedro Coutinho <pmd.coutinho@gmail.com>
2024-12-27 02:04:13 +0000

    automated dev commit

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -5,15 +5,13 @@ return {
   config = function()
     local harpoon = require 'harpoon'
 
-    harpoon:setup()
-
     vim.keymap.set('n', '<leader>A', function()
       harpoon:list():prepend()
     end)
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end)
-    vim.keymap.set('n', '<C-d>', function()
+    vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
